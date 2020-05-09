13414bc52d36af3cb78b4d539ac8b36dd5ada763
Priyobrato Das <priyodas12@gmail.com>
2020-05-09 19:29:02 +0530

    Initial commit

---
+++ movie-discovery-server/src/test/java/io/springlab/moviediscoveryserver/MovieDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springlab.moviediscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
